8d9327d678b9668c2dd2d96df9ccd2e7f91ccb14
ramsandhya <ram.sandhya@gmail.com>
2016-11-15 13:37:22 -0500

    UI Set up

---
+++ rainyShinyCloudy/Pods/Target Support Files/Pods-rainyShinyCloudy/Pods-rainyShinyCloudy-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_rainyShinyCloudyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_rainyShinyCloudyVersionString[];
+
---
+++ rainyShinyCloudy/Pods/Target Support Files/Pods-rainyShinyCloudyUITests/Pods-rainyShinyCloudyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rainyShinyCloudyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_rainyShinyCloudyUITests
+@end
